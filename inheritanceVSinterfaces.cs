9ed964583a4518d7c3b24ab2804bace10b30a42d
Harshavardhan Asode <hxvrdn@gmail.com>
2024-03-06 22:35:03 +0530

    Add project files.

---
+++ inheritanceVSinterfaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Advanced
+{
+    /* inheritance is not code sharing
+     * inheritance is a relationship
+     * inheritance needs to share common logic
+     * sharing just propertties or method signature is not enough
+     * inheritanc
+     */
+
+    internal class inheritanceVSinterfaces
+    {
+
+    }
+}
