d6542baa0dec0c48ebbaef76e4c667298491ad39
Harshavardhan Asode <hxvrdn@gmail.com>
2024-03-06 22:49:38 +0530

    We encountered the problem while trying to design a rental solution for newly added truck.

---
+++ RentalVehicle.cs
@@ -0,0 +1,22 @@
+﻿namespace C_Advanced
+{
+    public class Rentalvehicle
+    {
+        public int RentalId { get; set; }
+        public string Currentrenter { get; set; }
+        public decimal priceperday { get; set; }
+        public int numberofpassengers { get; set; }
+        public CarType Style { get; set; }
+
+        public void StratEngine()
+        {
+            Console.WriteLine(  "Turn key to ingnition setting");
+            Console.WriteLine(  "Turn key to on");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine( "Turn Key to off");
+        }
+    }
+}
--- inheritanceVSinterfaces.cs
+++ inheritanceVSinterfaces.cs
@@ -10,9 +10,19 @@ namespace C_Advanced
      * inheritance is a relationship
      * inheritance needs to share common logic
      * sharing just propertties or method signature is not enough
-     * inheritanc
      */
 
+
+    // Rental agency in Miami Florida
+    /*
+     * we used to rent cars
+     * we bought a truck for rental service 
+     * but now we have to inherit the properties of the the vehicles for cars and the trucks seperately.
+     * we have a enum by name "Cartype" which becomes incompatible in the context of truck.
+     * ----------------------That's The Problem here ------------------------------------------
+     * Further:
+     * 
+     * */
     internal class inheritanceVSinterfaces
     {
 
